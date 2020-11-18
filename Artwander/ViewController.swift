57bf421aef38df11df33bd44136a7f4f8a674287
Stefan Tanaskovic <tanaskos@sheridancollege.ca>
2020-11-18 18:10:33 -0500

    Initial Commit

---
+++ Artwander/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Artwander
+//
+//  Created by Stefan Tanaskovic on 2020-11-18.
+//  Copyright © 2020 Stefan Tanaskovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
