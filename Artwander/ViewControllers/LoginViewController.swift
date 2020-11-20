7c280c553e8a7b30d284fd383973f62df0aa89ac
Brian M <brian.mulhall@hotmail.com>
2020-11-20 00:16:47 -0500

    Login Functionality
    Added Basics of Login

--- Artwander/ViewControllers/LoginViewController.swift
+++ Artwander/ViewControllers/LoginViewController.swift
@@ -11,11 +11,53 @@ import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
+    var mainDelegate = UIApplication.shared.delegate as! AppDelegate
+
+    @IBOutlet var Email_tf : UITextField!
+    @IBOutlet var Password_tf : UITextField!
+
+    @IBAction func unwindToLogin(sender: UIStoryboardSegue) {}
+    @IBAction func completeLogin() {
+        self.doFirebaseLogin(username: Email_tf.text!, password: Password_tf.text!)
+    }
     
+    // MARK: ViewController Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
 
 
+    // MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        return true
+    }
+
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+
+    }
+
+    func doFirebaseLogin(username: String, password: String) {
+        Auth.auth().signIn(withEmail: username, password: password) { [weak self] authResult, error in
+            if error != nil {
+                self!.presentAlert(title: "Login Error", message: error!.localizedDescription, preferredStyle: .alert)
+            } else {
+                self!.mainDelegate.currentUserId = authResult?.user.uid
+                self!.mainDelegate.updateCurrentUser()
+                self!.performSegue(withIdentifier: "MainAppTabBar", sender: self)
+            }
+        }
+    }
+
+    func presentAlert(title: String, message: String, preferredStyle: UIAlertController.Style) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: preferredStyle)
+        let okayAction = UIAlertAction(title: "Okay", style: .default, handler: { (alert: UIAlertAction!) in
+            self.dismiss(animated: true, completion: nil)
+        })
+
+        alertController.addAction(okayAction)
+        present(alertController, animated: true)
+    }
 }
+
